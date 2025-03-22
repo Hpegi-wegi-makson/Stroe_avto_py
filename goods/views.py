072ed292e2a690f1c90f5e4365d2a3269a16bb52
Hpegi-wegi-makson <156901344+Hpegi-wegi-makson@users.noreply.github.com>
2025-03-22 20:05:30 +0300

    Second pagination option - GET params

--- goods/views.py
+++ goods/views.py
@@ -6,7 +6,11 @@ import goods
 from goods.models import Products
 
 
-def catalog(request,category_slug, page=1 ):
+def catalog(request,category_slug, ):
+
+    page = request.GET.get('page', 1)
+
+
 
     if category_slug =='all':
         goods=Products.objects.all()
@@ -14,7 +18,7 @@ def catalog(request,category_slug, page=1 ):
         goods=Products.objects.filter(category__slug=category_slug)
 
     paginator = Paginator(goods, 3,)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
     context = {
         'title': 'Поршень - Каталог',
         'goods': current_page,
