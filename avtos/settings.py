4d87cdd3f88f5a090e0b585abd619d2fdfc7897d
Hpegi-wegi-makson <156901344+Hpegi-wegi-makson@users.noreply.github.com>
2025-03-25 10:59:29 +0300

    PostgreSql data base connected & full text search

--- avtos/settings.py
+++ avtos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
 
     "debug_toolbar",
@@ -89,8 +90,12 @@ WSGI_APPLICATION = 'avtos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Porshen',
+        'USER': 'Porshen',
+        'PASSWORD': '2145',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
